64888b2a285a7ce90703cf013f6420393303e857
Vishal <66051441+greenhat-cyber@users.noreply.github.com>
2023-03-17 19:26:21 +0530

    teachers api

--- routes/teacher.js
+++ routes/teacher.js
@@ -3,43 +3,63 @@ const router = express.Router();
 const teachers = require("../modals/teacher");
 
 // POST
-router.post("/", async (req, res) => {
-    console.log("reqqq:", req.body);
-    const data = new teachers({
-        name: req.body.name,
-        age: req.body.age,
-        subject: req.body.subject,
-        qualification: req.body.qualification,
-    });
+router.post("/add", async (req, res) => {
+  console.log("reqqq:", req.body);
+  const data = new teachers({
+    name: req.body.name,
+    age: req.body.age,
+    subject: req.body.subject,
+    qualification: req.body.qualification,
+  });
 
-    try {
-        const t1 = await data.save();
-        res.send(t1);
-    } catch (error) {
-        res.send(error);
-    }
+  try {
+    const t1 = await data.save();
+    res.send(t1);
+  } catch (error) {
+    res.send(error);
+  }
 });
 
 // GET
 
-router.get('/',async(req,res)=>{
-    try {
-        const teacher = await teachers.find()
-        res.send(teacher)
-    } catch (error) {
-        res.send(error)
-    }
-})
+router.get("/list", async (req, res) => {
+  try {
+    const teacher = await teachers.find();
+    res.send(teacher);
+  } catch (error) {
+    res.send(error);
+  }
+});
 
 // Details GET
 
-router.delete('/:id',async(req,res)=>{
-    try {
-        const teacher = await teachers.findById(req.params.id)
-        res.send(teacher)
-    } catch (error) {
-        res.send(error)
-    }
-})
+router.get("/details/:id", async (req, res) => {
+  try {
+    const teacher = await teachers.findById(req.params.id);
+    res.send(teacher);
+  } catch (error) {
+    res.send(error);
+  }
+});
+
+//UPDATE
+
+router.patch("/update/:id", async (req, res) => {
+  let data = await teachers.updateOne(
+    { _id: req.params.id },
+    { $set: req.body }
+  );
+  res.send(data);
+});
+
+// DELETE
+
+router.delete("/delete/:id", async (req, res) => {
+  let data = await teachers.deleteOne(
+    { _id: req.params.id },
+    { $set: req.body }
+  );
+  res.send(data);
+});
 
 module.exports = router;
