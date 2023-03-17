f09d165abf2cee059339d85f98fbd4073787fca1
Vishal <66051441+greenhat-cyber@users.noreply.github.com>
2023-03-17 21:26:04 +0530

    teachers api

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const teacherRoute = require('./routes/teacher');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
 
 const app = express();
-const url = "mongodb://127.0.0.1:27017/university";
+const url = process.env.MONGODB_URI;
+// const url = "mongodb+srv://thisDeveloper:this.developer@cluster0.zyrgsbn.mongodb.net/?retryWrites=true&w=majority";
+// const url = "mongodb://127.0.0.1:27017/university";
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -17,6 +21,6 @@ mongoose.connect(url).then(() => {
 
 app.use('/teacher', teacherRoute);
 
-app.listen(5000, () => {
-  console.log('Server started on port 5000...');
+app.listen(process.env.PORT, () => {
+  console.log(`Server started on port ${process.env.PORT}...`);
 });
